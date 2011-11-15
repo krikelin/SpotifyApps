63a2244f82467e5cd1ce2b2343ae1c8a9864327d
unknown <Alexander@Alexander-Dator.(none)>
2011-11-15 10:39:57 +0100

    Signed-off-by: unknown <Alexander@Alexander-Dator.(none)>

---
+++ src/com/krikelin/spotifysource/AppInstaller.java
@@ -0,0 +1,136 @@
+package com.krikelin.spotifysource;
+import java.io.BufferedWriter;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.jar.JarEntry;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+
+
+public class AppInstaller {
+	protected void copyFile(String src,String dest) throws IOException{
+		File inputFile = new File(src);
+	    File outputFile = new File(dest);
+
+	    FileReader in = new FileReader(inputFile);
+	    FileWriter out = new FileWriter(outputFile);
+	    int c;
+
+	    while ((c = in.read()) != -1)
+	      out.write(c);
+
+	    in.close();
+	    out.close();
+	}
+	protected void extractJar(String destDir,String jarFile) throws IOException{
+		File destination  = new File(destDir);
+		java.util.jar.JarFile jar = new java.util.jar.JarFile(jarFile);
+		java.util.Enumeration<JarEntry> enu = jar.entries();
+		while (enu.hasMoreElements()) {
+			try{
+			    java.util.jar.JarEntry file = (java.util.jar.JarEntry) enu.nextElement();
+			    java.io.File f = new java.io.File(destDir + java.io.File.separator + file.getName());
+			    if (file.isDirectory()) { // if its a directory, create it
+			        f.mkdir();
+			        continue;
+			    }
+			    java.io.InputStream is = jar.getInputStream(file); // get the input stream
+			    if(!(destination.exists())){
+			    	destination.createNewFile();
+			    }
+			   
+			    java.io.FileOutputStream fos = new java.io.FileOutputStream(f);
+			    while (is.available() > 0) {  // write contents of 'is' to 'fos'
+			        fos.write(is.read());
+			    }
+			    fos.close();
+			    is.close();
+			}catch(Exception e){
+				e.printStackTrace();
+			}
+		}
+	}
+	protected void addPackage(String file,String context) throws IOException{
+		// Add the package to the file
+	
+		BufferedWriter bw = new BufferedWriter(new FileWriter(SPContainer.EXTENSION_DIR+"\\"+context, true));
+		bw.write("\n"+file);
+		bw.close();
+	}
+	public static String xmlns = "http://spotiapps/";
+	public static final String tmp_dir = "C:\\temp\\";
+	public void installApp(File app_jar) throws IOException, SAXException, ParserConfigurationException{
+
+	
+		String tmp_app_dir = tmp_dir +app_jar.getName() + "\\";
+		
+		java.util.jar.JarFile jar = new java.util.jar.JarFile(app_jar);
+		// Get manifest
+		java.util.Enumeration<JarEntry> enu = jar.entries();
+		while(enu.hasMoreElements()){
+			JarEntry elm = enu.nextElement();
+			if(elm.getName().equals("SpotifyAppManifest.xml")){
+				DataInputStream dis = new DataInputStream(jar.getInputStream(elm));
+				Document d = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(dis);
+				dis.close();
+				// Get package name
+				String package_name = d.getDocumentElement().getAttribute("package");
+				
+				addPackage(package_name,"packages");
+				// Get all activities
+				NodeList activities = d.getElementsByTagName("activity");
+				for(int i=0; i < activities.getLength(); i++){
+					Element activity = (Element)activities.item(i);
+					String name = activity.getAttribute("name"); // Get the name
+					// Append the previous name if it starts with .
+					if(name.startsWith(".")){
+						name = name.replace(".", "");
+						
+					}
+					if(!name.startsWith(package_name)){
+						name = package_name + "." + name;
+						
+					}
+					//addPackage(name,"activities");
+					NodeList intentFilters = activity.getElementsByTagName("intent-filter");
+					for(int j=0; j < intentFilters.getLength(); j++){
+						NodeList actions =  ((Element)intentFilters.item(0)).getElementsByTagName("action");
+						for(int k=0; k < actions.getLength(); k++){
+							String action_name = ((Element)actions.item(k)).getAttribute("name");
+							addPackage(name,"\\activities\\"+ action_name);
+						}
+						
+					}
+				}
+				copyFile(app_jar.getAbsolutePath(), SPContainer.EXTENSION_DIR+"\\jar\\"+package_name+".jar");
+				// Runtime.getRuntime().exec("copy \""+app_jar+"\" \""+SPContainer.EXTENSION_DIR+"\\jar\\"+package_name+".jar\"");
+				
+			}
+		}
+		
+		
+		
+	}
+	public static void main(String[] args){
+		try {
+			new AppInstaller().installApp(new File("C:\\twittify.jar"));
+		} catch (IOException | SAXException | ParserConfigurationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
